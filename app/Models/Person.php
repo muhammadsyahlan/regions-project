a54152924865b0ed97ef625505a5f6259fab0139
Farhan Anshari Miraj <farhananshari77@gmail.com>
2021-10-12 13:30:11 +0700

    update migration personal

---
+++ app/Models/Person.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    use HasFactory;
+
+    protected $table="personal";
+    protected $fillable=["id","prov_id","kotkab_id","nama","gender","goldar","hobby","alamat","keterangan","created_at","updated_at"];
+    
+    public function prov()
+    {
+        return $this->belongsTo(Prov::class);
+    }
+
+    public function kotkab()
+    {
+        return $this->belongsTo(Koka::class);
+    }
+}
